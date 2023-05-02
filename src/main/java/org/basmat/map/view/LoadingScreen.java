532cefbae8929b14721c8f9288793d12e6e1f465
epicbasmat <43749261+epicbasmat@users.noreply.github.com>
2023-05-02 02:32:02 +0100

    loading icon is now rendered

--- src/main/java/org/basmat/map/view/LoadingScreen.java
+++ src/main/java/org/basmat/map/view/LoadingScreen.java
@@ -4,6 +4,8 @@ import org.basmat.map.util.ECellType;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Objects;
@@ -13,20 +15,21 @@ import java.util.Objects;
 Thanks to https://icons8.com/preloaders/ for providing the icon used for the loading screen
  */
 public class LoadingScreen extends JPanel {
-
     public LoadingScreen() {
-        this.validate();
         this.setLayout(new BorderLayout());
         JFrame layout = new JFrame();
         layout.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-        Icon image = new ImageIcon("./assets/loading.gif");
-        JLabel comp = new JLabel(image);
-        this.add(comp, BorderLayout.CENTER);
-        this.setVisible(true);
-    }
-
-    public void setInfoForUser() {
-
+        try {
+            InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("textures/loading.gif");
+            byte[] imageData = new byte[Objects.requireNonNull(resourceAsStream).available()];
+            resourceAsStream.read(imageData);
+            Icon image = new ImageIcon(imageData);
+            JLabel comp = new JLabel(image);
+            this.add(comp, BorderLayout.CENTER);
+            this.setVisible(true);
+            this.validate();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
